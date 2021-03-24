610d42d861c390bfb8570b401a52b98301a14a25
FazioNico <contact@nicolasfazio.ch>
2021-03-24 12:47:02 +0100

    upd: use pipe Level

--- src/app/features/detail/detail.module.ts
+++ src/app/features/detail/detail.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
 import { IonicModule } from '@ionic/angular';
+import { LevelPipe } from 'src/app/pipes/level/level.pipe';
 
 
 @NgModule({
-  declarations: [DetailComponent],
+  declarations: [
+    DetailComponent,
+    LevelPipe
+  ],
   imports: [
     CommonModule,
     DetailRoutingModule,
