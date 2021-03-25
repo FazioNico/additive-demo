083533676cc5c397e7217df81f482319df578464
FazioNico <contact@nicolasfazio.ch>
2021-03-25 12:33:07 +0100

    upd: creat shared Header Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PIPES } from './pipes';
+import { COMPONENTS } from './components';
+import { IonicModule } from '@ionic/angular';
+
 
 @NgModule({
   declarations: [
-    ...PIPES
+    ...PIPES,
+    ...COMPONENTS
   ],
   imports: [
+    IonicModule,
     CommonModule
   ],
   exports: [
-    ...PIPES
+    ...PIPES,
+    ...COMPONENTS
   ]
 })
 export class SharedModule { }
