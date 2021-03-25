5fe30387e696a787ac092e3eb1cec92877cad0af
FazioNico <contact@nicolasfazio.ch>
2021-03-25 09:59:04 +0100

    upd: use AlertController

--- src/app/services/additive.service.ts
+++ src/app/services/additive.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IAdditive } from '../interfaces/additive';
+import { catchError } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,8 @@ import { IAdditive } from '../interfaces/additive';
 export class AdditiveService {
 
   constructor(
-    private _http: HttpClient
+    private _http: HttpClient,
+    private _alert: AlertController
   ) { }
 
   getAll(): Observable<IAdditive[]> {
@@ -18,7 +21,19 @@ export class AdditiveService {
 
 
   getById(id: string): Observable<IAdditive> {
-    return this._http.get<IAdditive>(`http://localhost:3000/additives/${id}`);
+    return this._http.get<IAdditive>(`http://localhost:3000/additives/${id}`).pipe(
+      catchError(async (err) => {
+        const ionAlert = await this._alert.create({
+          header: 'Erreur',
+          message: err?.statusText || 'Une ereur est survenue...',
+          buttons: [
+            {text: 'ok'}
+          ]
+        });
+        await ionAlert.present();
+        throw err;
+      })
+    );
   }
 
 }
