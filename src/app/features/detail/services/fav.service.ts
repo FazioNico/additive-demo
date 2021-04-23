a77a1ad98046480b943245551f6d49812ef2a296
FazioNico <contact@nicolasfazio.ch>
2021-04-23 10:20:55 +0200

    upd: use user auth uid

--- src/app/features/detail/services/fav.service.ts
+++ src/app/features/detail/services/fav.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { first, map, switchMap } from 'rxjs/operators';
 import { IAdditive } from '../../../interfaces/additive';
 
 @Injectable()
@@ -11,20 +12,29 @@ export class FavService {
   favs$ = this._fav$.asObservable();
 
   constructor(
-    private _firestore: AngularFirestore
+    private _firestore: AngularFirestore,
+    private _fireauth: AngularFireAuth
   ) {
     console.log('[INFO] init FavService...');
-    this._firestore
-      .collection<IAdditive & {key: string}>('fav-additive')
-      .stateChanges(['added'])
-      .pipe(
-        map(actions => actions.map(a => {
-          const key = a.payload.doc.id;
-          const data = a.payload.doc.data();
-          return {key, ...data};
-        }))
-      )
-      .subscribe(
+    this._fireauth.user.pipe(
+      map(user => user?.uid),
+      switchMap(uid => {
+        return this._firestore
+        .collection<IAdditive & {key: string}>(
+          'fav-additive',
+          ref => ref.where('uid', '==', uid)
+        )
+        .stateChanges(['added'])
+        .pipe(
+          map(actions => actions.map(a => {
+            const key = a.payload.doc.id;
+            const data = a.payload.doc.data();
+            return {key, ...data};
+          }))
+        )
+      })
+    )
+    .subscribe(
         res => {
           const currentState = this._fav$.value.filter(
             additive => !res.find(newA => newA.id === additive.id)
@@ -36,27 +46,37 @@ export class FavService {
           this._fav$.next(newState);
         }
       );
-    this._firestore
-      .collection<IAdditive & {key: string}>('fav-additive')
-      .stateChanges(['removed'])
-      .pipe(
-        map(actions => actions.map(a => {
-          const key = a.payload.doc.id;
-          const data = a.payload.doc.data();
-          return {key, ...data};
-        }))
-      )
-      .subscribe(
-        res => {
-          const currentState = this._fav$.value.filter(
-            additive => !res.find(newA => newA.id === additive.id)
-          );
-          const newState = [
-            ...currentState
-          ];
-          this._fav$.next(newState);
-        }
-      );
+
+    this._fireauth.user
+    .pipe(
+      map(user => user?.uid),
+      switchMap(uid => {
+        return this._firestore
+        .collection<IAdditive & {key: string}>(
+          'fav-additive',
+          ref => ref.where('uid', '==', uid)
+        )
+        .stateChanges(['removed'])
+        .pipe(
+          map(actions => actions.map(a => {
+            const key = a.payload.doc.id;
+            const data = a.payload.doc.data();
+            return {key, ...data};
+          }))
+        )
+      })
+    )
+    .subscribe(
+      res => {
+        const currentState = this._fav$.value.filter(
+          additive => !res.find(newA => newA.id === additive.id)
+        );
+        const newState = [
+          ...currentState
+        ];
+        this._fav$.next(newState);
+      }
+    );
   }
 
   isFav$(id: string): Observable<boolean> {
@@ -67,7 +87,13 @@ export class FavService {
   }
 
   async add(fav: {id: string}): Promise<void> {
-    await this._firestore.collection('fav-additive').add(fav);
+    const {uid = null} = await this._fireauth.currentUser;
+    this._firestore
+        .collection('fav-additive')
+        .add({
+          ...fav,
+          uid
+        });
   }
 
   async remove(id: string): Promise<void> {
